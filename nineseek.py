ecc80673a53d9178c1210b3594bf258700b62f53
Luyu <x86@u.nus.edu>
2026-04-22 14:26:33 +0800

    v0.1

--- nineseek.py
+++ nineseek.py
@@ -53,9 +53,9 @@ def show_result(scores):
     
 if st.button("评估", type="primary", width='stretch'):
     kvs = {**D1_data, **D2_data, **D3_data, **D4_data, **D5_data, **D6_data}
-    st.write(kvs)
+    # st.write(kvs)
     DB_utils.record(kvs)
     show_result(scores)
 
 df = DB_utils.query("SELECT * FROM proj", ttl=0)
-st.dataframe(df)
+# st.dataframe(df)
